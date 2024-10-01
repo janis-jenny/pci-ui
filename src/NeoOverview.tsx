7376cadfab1acdb863327991ed9e538c90ad731b
janis-jenny <pcardenasvm@gmail.com>
2024-10-01 18:33:32 -0500

    Task #2

--- src/NeoOverview.tsx
+++ src/NeoOverview.tsx
@@ -1,3 +1,4 @@
+import React, { useMemo } from 'react';
 import { AgGridReact } from "ag-grid-react";
 import { ColDef } from "ag-grid-community";
 import data from "./near-earth-asteroids.json";
@@ -5,20 +6,19 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import './index.css';
 
-const columnDefs: ColDef[] = [
-  { field: "designation", headerName: "Designation" },
-  { field: "discovery_date", headerName: "Discovery Date" },
-  { field: "h_mag", headerName: "H (mag)" },
-  { field: "moid_au", headerName: "MOID (au)" },
-  { field: "q_au_1", headerName: "q (au)" },
-  { field: "q_au_2", headerName: "Q (au)" },
-  { field: "period_yr", headerName: "Period (yr)" },
-  { field: "i_deg", headerName: "Inclination (deg)" },
-  { field: "pha", headerName: "Potentially Hazardous" },
-  { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, },
-];
-
 const NeoOverview = (): JSX.Element => {
+  const columnDefs: ColDef[] = useMemo(() => [
+    { field: "designation", headerName: "Designation", filter: 'agTextColumnFilter', sortable: true },
+    { field: "discovery_date", headerName: "Discovery Date", filter: 'agDateColumnFilter', sortable: true },
+    { field: "h_mag", headerName: "H (mag)", filter: 'agNumberColumnFilter', sortable: true, valueParser: (params) => Number(params.newValue), },
+    { field: "moid_au", headerName: "MOID (au)", filter: 'agNumberColumnFilter', sortable: true, valueParser: (params) => Number(params.newValue),},
+    { field: "q_au_1", headerName: "q (au)", filter: 'agNumberColumnFilter', sortable: true, valueParser: (params) => Number(params.newValue),},
+    { field: "q_au_2", headerName: "Q (au)", filter: 'agNumberColumnFilter', sortable: true, valueParser: (params) => Number(params.newValue),},
+    { field: "period_yr", headerName: "Period (yr)", filter: 'agNumberColumnFilter', sortable: true, valueParser: (params) => Number(params.newValue),},
+    { field: "i_deg", headerName: "Inclination (deg)", filter: 'agNumberColumnFilter', sortable: true, valueParser: (params) => Number(params.newValue),},
+    { field: "pha", headerName: "Potentially Hazardous", filter: 'agTextColumnFilter', sortable: true, },
+    { field: "orbit_class", headerName: "Orbit Class", enableRowGroup: true, filter: 'agTextColumnFilter', sortable: true },
+  ], []);
   return (
     <div className="neo-overview">
       <header>
@@ -29,6 +29,8 @@ const NeoOverview = (): JSX.Element => {
           rowData={data}
           columnDefs={columnDefs}
           rowGroupPanelShow={'always'}
+          multiSortKey="ctrl"
+          suppressMultiSort={true}
         />
       </div>
     </div>
