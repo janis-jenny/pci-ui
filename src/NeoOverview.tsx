8628ebdf67f766504541aee14e21aa695f48e036
janis-jenny <pcardenasvm@gmail.com>
2024-10-02 00:20:57 -0500

    Task #3

--- src/NeoOverview.tsx
+++ src/NeoOverview.tsx
@@ -6,10 +6,31 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import './index.css';
 
-const NeoOverview = (): JSX.Element => {
+const NeoOverview: React.FC = () => {
+  const formatDate = (dateString: string): string => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-GB');
+  };
   const columnDefs: ColDef[] = useMemo(() => [
     { field: "designation", headerName: "Designation", filter: 'agTextColumnFilter', sortable: true },
-    { field: "discovery_date", headerName: "Discovery Date", filter: 'agDateColumnFilter', sortable: true },
+    { field: "discovery_date", headerName: "Discovery Date", filter: 'agDateColumnFilter', sortable: true,
+      filterParams: {
+        comparator: (filterLocalDateAtMidnight: Date, cellValue: string) => {
+          const cellDate = new Date(cellValue);
+
+          if (filterLocalDateAtMidnight.getTime() === cellDate.getTime()) {
+            return 0;
+          }
+          if (cellDate < filterLocalDateAtMidnight) {
+            return -1;
+          }
+          if (cellDate > filterLocalDateAtMidnight) {
+            return 1;
+          }
+        },
+        browserDatePicker: true,
+        },
+      valueFormatter: (params) => formatDate(params.value) },
     { field: "h_mag", headerName: "H (mag)", filter: 'agNumberColumnFilter', sortable: true, valueParser: (params) => Number(params.newValue), comparator: (valueA, valueB) => valueA - valueB},
     { field: "moid_au", headerName: "MOID (au)", filter: 'agNumberColumnFilter', sortable: true, valueParser: (params) => Number(params.newValue), comparator: (valueA, valueB) => valueA - valueB},
     { field: "q_au_1", headerName: "q (au)", filter: 'agNumberColumnFilter', sortable: true, valueParser: (params) => Number(params.newValue), comparator: (valueA, valueB) => valueA - valueB},
@@ -29,7 +50,6 @@ const NeoOverview = (): JSX.Element => {
           rowData={data}
           columnDefs={columnDefs}
           rowGroupPanelShow={'always'}
-          multiSortKey="ctrl"
           suppressMultiSort={true}
         />
       </div>
